f1139901c5fc27f49e023f84d3b5b63db928f393
Tobias Konzok <konzok.tobias@gmail.com>
2023-06-21 19:28:10 +0200

    Add explanatory comments

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 console.log("Hello World");
 
+/* List of possible choices */
 const choices = ["rock", "paper", "scissors"];
 
+/* Function picks random choice out of the list of possible choices */
 function getComputerChoice() {
     let randomPickIndex = Math.floor(Math.random() * choices.length);
     return choices[randomPickIndex].toString();
 }
 
+/* Promt asks user to choose either rock, paper or scissors */
 function getPlayerChoice() {
     choice = prompt("Enter rock, paper or scissors: ");
     return choice;
 }
 
+/* Simulation of one round of the game. Compares both choices and returns the winner or tie, if it's a tie */
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Tie";
-    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+    } else if (
+        playerSelection === "rock" && computerSelection === "scissors" || 
+        playerSelection === "scissors" && computerSelection === "paper" || 
+        playerSelection === "paper" && computerSelection === "rock"
+        ) {
         return "Player";
     } else {
         return "Computer";
     }
 }
 
-// console.log(playRound(playerSelection, computerSelection));
-
+/* Full game. Plays as many rounds as inserted as parameter roundsToPlay */
 function game(roundsToPlay) {
     let roundsPlayed = 0;
     let score = 0;
@@ -54,4 +61,5 @@ function game(roundsToPlay) {
 
 }
 
+/* Calling the game function. Parameter tells game how many rounds to be played. */
 game(5)
