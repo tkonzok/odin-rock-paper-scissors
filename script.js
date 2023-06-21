e5934596577221fda2f8fe881efbe715d213262f
Tobias Konzok <konzok.tobias@gmail.com>
2023-06-21 19:15:36 +0200

    Code game functionality to play 5 rounds.

--- script.js
+++ script.js
@@ -7,18 +7,51 @@ function getComputerChoice() {
     return choices[randomPickIndex].toString();
 }
 
-let playerSelection = "Rock";
-let computerSelection = getComputerChoice();
+function getPlayerChoice() {
+    choice = prompt("Enter rock, paper or scissors: ");
+    return choice;
+}
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return `You both selected ${computerSelection}, it's a tie.`;
+        return "Tie";
     } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-        return `You selected ${playerSelection} while the computer selected ${computerSelection}, you win!`;
+        return "Player";
     } else {
-        return `You selected ${playerSelection} while the computer selected ${computerSelection}, you lose!`;
+        return "Computer";
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+function game(roundsToPlay) {
+    let roundsPlayed = 0;
+    let score = 0;
+    let playerSelection = "";
+    let computerSelection = "";
+    let winner = "";
+
+    while (roundsPlayed < roundsToPlay) {
+        playerSelection = getPlayerChoice().toLowerCase();
+        console.log(playerSelection);
+        computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        winner = playRound(playerSelection, computerSelection);
+        if (winner === "Tie") {
+            alert(`You both selected ${computerSelection}, it's a tie.`);
+        } else if (winner === "Player") {
+            score++
+            alert(`You selected ${playerSelection} while the computer selected ${computerSelection}, you win!`);
+        } else {
+            alert(`You selected ${playerSelection} while the computer selected ${computerSelection}, you lose!`);
+        }
+        roundsPlayed++;
+        alert(`Current score: ${score}`);
+        console.log(roundsPlayed);
+    }
+
+    alert(`Game over! You played ${roundsPlayed} rounds against the computer and won ${score} rounds of it.`);
+
+}
+
+game(5)
